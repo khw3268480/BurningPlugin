3dafdb9944bac9a90bb646c95c8d85df8ad0cd92
CodingApe9 <pos06098@gmail.com>
2023-07-23 19:03:42 +0900

    Initial commit

---
+++ src/main/java/Burning.java
@@ -0,0 +1,13 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Burning extends JavaPlugin {
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
